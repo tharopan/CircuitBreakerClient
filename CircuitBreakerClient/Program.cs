114a640be1a0c677768a6872c2f8b50954ca4444
tharo <tharopan@gmail.com>
2019-07-26 17:46:20 +0530

    Initial commit

---
+++ CircuitBreakerClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CircuitBreakerClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel Come to Circuit Breaker Client\n--------------------------------------");
+            int index = 0;
+            while (index < 100)
+            {
+                Console.WriteLine(index);
+                CircuitBreakerClient("http://localhost", "cartid/9817ee95-8c97-4317-9662-a01785b00e7f");
+                index++;
+            }
+
+            Console.ReadKey();
+        }
+
+
+        public static async Task CircuitBreakerClient(string baseUrl, string apiMethod)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                ////GET Method "api/Department/1"
+                //HttpResponseMessage response = await client.GetAsync(apiMethod);
+
+                //if (response.IsSuccessStatusCode)
+                //{
+                //    var resp = await response.Content.ReadAsStringAsync();
+                //    Console.WriteLine(resp);
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Internal server Error");
+                //}
+
+                Console.WriteLine("Internal server Error");
+            }
+        }
+    }
+}
